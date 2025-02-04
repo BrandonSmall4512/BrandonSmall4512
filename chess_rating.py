9e84b640987e81a0f0e6f5737c417c0faa0e677c
Степан <144131544+BrandonSmall4512@users.noreply.github.com>
2025-02-04 11:21:13 +0300

    Update chess_rating.py

--- chess_rating.py
+++ chess_rating.py
@@ -4,26 +4,45 @@ from datetime import datetime
 
 USERNAME = "Brandon_small"
 
-
 url = f"https://api.chess.com/pub/player/{USERNAME}/games/archives"
 response = requests.get(url)
-archives = response.json()["archives"]
+
+
+if response.status_code == 200:
+    data = response.json()
+
+    
+    if "archives" in data:
+        archives = data["archives"]
+    else:
+        print("No archives found.")
+        exit(1)
+else:
+    print(f"Error: {response.status_code}")
+    print(response.text)
+    exit(1)
 
 games = []
-for archive_url in archives[-3:]:  
-    archive_data = requests.get(archive_url).json()
-    games.extend(archive_data["games"])
+for archive_url in archives[-3:]:
+    archive_response = requests.get(archive_url)
+    
+ 
+    if archive_response.status_code == 200:
+        archive_data = archive_response.json()
+        games.extend(archive_data["games"])
+    else:
+        print(f"Failed to fetch archive: {archive_url}")
 
 ratings = []
 dates = []
 
-for game in games[-100:]:  
+for game in games[-100:]:
     if "pgn" not in game:
         continue
 
     rating = game["white"]["rating"] if game["white"]["username"].lower() == USERNAME.lower() else game["black"]["rating"]
     game_date = datetime.fromtimestamp(game["end_time"])
-    
+
     ratings.append(rating)
     dates.append(game_date)
 
